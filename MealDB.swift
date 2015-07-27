2480955233f3b71f554b2381d5db4f951c9c2cd5
LMorgado <leandro-morg@hotmail.com>
2015-07-27 00:18:17 -0300

    atualizando Banco....

--- MealDB.swift
+++ MealDB.swift
@@ -12,10 +12,9 @@ import UIKit
 
 class MealDB {
     
-    
-/* Metodo estatico que cria uma instancia de Meal. E insere o objeto no contexto de persistencia.
- * Para instanciar um objeto Meal use : "MealDB.newInstance()"
- */
+    /* Metodo estatico que cria uma instancia de Meal. E insere o objeto no contexto de persistencia.
+    * Para instanciar um objeto Meal use : "MealDB.newInstance()"
+    */
     class func newInstance() -> Meal
     {
         // AppDelegate da aplicacao
@@ -30,9 +29,9 @@ class MealDB {
     }
     
     
-/* Retorna todas as refeicoes salvas no Banco de Dados em ordem decrescente de datas.
- * Retorna um array de Meals, ordenado por datas == ID's, em ordem decrescente.
- */
+    /* Retorna todas as refeicoes salvas no Banco de Dados por ORDEM DECRESCENTE de datas.
+    * Exemplo: em qualquer classe faça " let array = MealDB().getMeals()"
+    */
     func getMeals() -> Array<Meal>
     {
         // AppDelegate da aplicacao
@@ -48,10 +47,9 @@ class MealDB {
         let request = NSFetchRequest()
         request.entity = entity
         
-        //Ordenar a consulta por timeStamp
+        //Ordenar a consulta por datas == timeStamp em ORDEM DECRESCENTE.
         let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
         let sortDescriptors = [sortDescriptor]
-        
         request.sortDescriptors = sortDescriptors
         
         //Executa a consulta
@@ -61,21 +59,17 @@ class MealDB {
         if (array == nil) {
             
             println("Erro \(error)")
-            
             return [] as Array<Meal>
-            
         }
         
         return array as! Array<Meal>
-        
-        
     }
     
     
-/* Salva uma nova refeicao no Banco. Ou atualiza, se já existe ID (== timeStamp)
- *
- * Ao salvar a nova refeicao, é gravada a data (ou timeStamp) do processo (== ID da refeicao).
- */
+    /* Salva uma nova refeicao no Banco. Ou atualiza, se já existe certa data == ID (== timeStamp).
+    * Ao salvar a nova refeicao, é gravada a data (ou timeStamp) do processo (== ID da refeicao).
+    * Exemplo: "MealDB().save(novaRefeicao)"
+    */
     func save (meal: Meal)
     {
         // AppDelegate da aplicacao
@@ -97,13 +91,12 @@ class MealDB {
         if(!ok){
             println("Erro \(error)")
         }
-        
-        //println("Meal \(meal.title) salvo com sucesso.")
     }
-
     
-/* Deleta uma refeicao do Banco.
- */
+    
+    /* Deleta uma refeicao do Banco.
+    * Exemplo: MealDB().delete(qualquerRefeicao)
+    */
     func delete(meal: Meal)
     {
         // AppDelegate da aplicacao
@@ -125,59 +118,65 @@ class MealDB {
         
     }
     
-/* Dado o ano, funcao retorna o vetor com as datas distintas (e completas) das refeicoes. Ordem decrescente.
- * Retorna Array de NSDateComponents - datas distintos em dia, mes e ano.
- * Exemplo 1: "let MealDatesArray = MealDB().getEveryMealDays(2015)"
- * Exemplo 2: "println(daysMealArray[i].day)"
- */
+    /* Retorna vetor com ATE 30 datas distintas (e completas) das refeicoes MAIS RECENTES. Portanto, aqui, datas em ORDEM DECRESCENTE.
+    * Exemplo: "let MealDatesArray = MealDB().getEveryMealDates()"
+    */
     func getEveryMealDates () -> Array<NSDateComponents>
     {
         
         var array = [NSDateComponents]()
         var meals = [Meal]()
         
-        
+        // Preparando o CoreData para ser monitorado.
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let context = appDelegate.managedObjectContext!
         let entity = NSEntityDescription.entityForName("Meal", inManagedObjectContext: context)
-        
-        
         var fetchRequest = NSFetchRequest()
         fetchRequest.entity = entity
         
+        // Ordenar a consulta por datas == timeStamp em ORDEM DECRESCENTE.
+        let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
+        let sortDescriptors = [sortDescriptor]
+        fetchRequest.sortDescriptors = sortDescriptors
+        
+        // Obendo o vetor de todas as refeicoes cadastradas no Banco.
         meals = context.executeFetchRequest(fetchRequest, error: nil) as! [Meal]
         
         
+        // Varrendo o vetor meals.
         for meal in meals {
             
+            // Converte NSDate em suas componentes "dia, mes e ano", todos valores inteiros.
             let calendar = NSCalendar.currentCalendar()
             let components = calendar.components(.CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitYear, fromDate: meal.timeStamp)
-            
             let day = components.day
             let month = components.month
             let year = components.year
             
-            if( array.count == 0) {
+            // Construindo o vetor de datas. Array de NSDateComponents
+            if(array.count == 0) {
                 
                 var dateFound = NSDateComponents()
                 dateFound.day = day
                 dateFound.month = month
                 dateFound.year  = year
                 
-                array.append(dateFound)
+                println(dateFound.day)
                 
+                array.append(dateFound)
                 
             } else {
                 
-                if ((day != array[array.count - 1].day) || (month != array[array.count - 1].month) || (year != array[array.count - 1].year)) {
-                    
-                    var dateFound = NSDateComponents()
-                    dateFound.day = day
-                    dateFound.month = month
-                    dateFound.year  = year
-                    
-                    array.append(dateFound)
-                    
+                if ((day != array[array.count - 1].day)    ||
+                    (month != array[array.count - 1].month) ||
+                    (year != array[array.count - 1].year))  {
+                        
+                        var dateFound = NSDateComponents()
+                        dateFound.day = day
+                        dateFound.month = month
+                        dateFound.year  = year
+                        
+                        array.append(dateFound)
                 }
             }
             
@@ -186,47 +185,28 @@ class MealDB {
         }
         
         return array
-      
     }
     
-
     
     
-    
-    
-
-   /*  Funcao comparacao para ordenacao do vetor de Meals
+    /* Busca refeicoes por data de cadastro no Banco. Retorna vetor de Meals por ORDEM CRESCENTE de tempo (horas, minutos e segundos distintos).
+    * onde let MealDatesArray = MealDB().getMealsByDate(dataPesquisada)" - ver funcao acima.
     */
-    func acsOrder(meal1: Meal, meal2: Meal) -> Bool {
-   
-        return meal1.timeStamp.compare(meal2.timeStamp) == .OrderedAscending
-    }
-    
-    
-
-/* Busca refeicoes por data. Ordem crescente.
- * Retorna vetor de Meals em uma data específica (parametro NSDateComponents).
- * Exemplo: "let refeicoes = MealDB().getMealsByDate(MealDatesArray[0]),
- * onde let MealDatesArray = MealDB().getEveryMealDates(2015)" - ver funcao acima.
- */
     func getMealsByDate(data: NSDateComponents) -> Array<Meal>
     {
-        // Vetor com as refeicoes procuradas.
-        var array = [Meal]()
         
+        var array = [Meal]()
         var meals = [Meal]()
         
-        
+        // Preparando o CoreData para ser monitorado.
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let context = appDelegate.managedObjectContext!
         let entity = NSEntityDescription.entityForName("Meal", inManagedObjectContext: context)
-        
-        
         var fetchRequest = NSFetchRequest()
         fetchRequest.entity = entity
         
+        // Obendo o vetor de todas as refeicoes cadastradas no Banco.
         meals = context.executeFetchRequest(fetchRequest, error: nil) as! [Meal]
-        
         
         // Monitorando o Banco em busca das refeicoes.
         for meal in meals {
@@ -243,16 +223,17 @@ class MealDB {
             if (day == data.day && month == data.month && year == data.year) {
                 
                 array.append(meal)
-                
             }
             
-        } // Fim da construcao do vetor de refeicoes distintas na mesma data.
+        } // Fim da construcao do vetor de refeicoes distintas para uma certa data.
         
-        // Devolve array em ordem crescente por data
-        return sorted(array, acsOrder)
+        // Devolve array em ORDEM CRESCENTE por data
+        return array
         
     }
     
- 
+    
+    /***************************************************************** FIM DAS FUNCOES DO BANCO LOCAL COREDATE *******************************************************************************/
+    
+    
 }
-
